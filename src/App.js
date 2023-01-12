046a329d4b799317d90ed9f82557b23d8bc3f3f4
Chizycodes <okwudirejoy@gmail.com>
2023-01-12 13:57:50 +0100

    file structure and route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="bg-[#FFFFFF]">
+			<Routes>
+				<Route path="/" element={<Auth />} />
+				<Route path="/auth" element={<Auth />} />
+				{/* <Route path="/dashboard/" element={<VirtualAccount />} /> */}
+				{/* <Route path="/dashboard/virtual-account" element={<VirtualAccount />} /> */}
+
+				<Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		colors: {
+			primary: '#B91C1C',
+		},
 		extend: {},
 	},
 	plugins: [],
