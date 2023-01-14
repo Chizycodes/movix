b0780e8fd7acc2b389a39f2bce512223f28a48ee
Chizycodes <okwudirejoy@gmail.com>
2023-01-14 14:32:39 +0100

    implemented video component

---
+++ src/components/home/movies/Video.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Video = () => {
+	return (
+		<div>
+			<div className="w-[449px] relative">
+				<img className="w-full h-[253px]" src="/images/video-thumbnail.svg" alt="movie" />
+				<div className="bg-[#fff] py-3">
+					<h2 className="text-[#111827] font-bold text-[18px]">Lamb (2021) Trailer</h2>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Video;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
+import MoviesSlide from '../components/home/movies/MoviesSlide';
 
 const HomePage = () => {
 	return (
 		<div className="font-DMSans">
 			<Hero />
+			<section className="max-w-[1280px] mx-auto py-16">
+				<div className="mb-20">
+					<MoviesSlide type="movies" title="Featured Movie" />
+				</div>
+
+				<div className="mb-20">
+					<MoviesSlide type="movies" title="New Arrival" />
+				</div>
+
+				<div className="mb-20">
+					<MoviesSlide type="videos" title="Exclusive Videos" />
+				</div>
+
+				<div className="mb-20">
+					<MoviesSlide type="casts" title="Featured Casts" />
+				</div>
+			</section>
 		</div>
 	);
 };
