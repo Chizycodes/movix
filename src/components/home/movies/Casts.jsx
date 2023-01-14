7e300be1e52e3ceee9ab882b16b3aeedb8c73358
Chizycodes <okwudirejoy@gmail.com>
2023-01-14 14:32:54 +0100

    created cast component

---
+++ src/components/home/movies/Casts.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Casts = () => {
+  return (
+		<div>
+			<div className="w-[250px] relative">
+				<img className="w-full h-[370px]" src="/images/poster.svg" alt="cast" />
+				<div className="bg-[#fff] py-3">
+					<h2 className="text-[#111827] font-bold text-[18px]">Keanu Reeves</h2>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Casts
